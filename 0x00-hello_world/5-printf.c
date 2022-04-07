870c0d747e46552314161661c6ec292ebe5bedb3
Abudi-seid10 <abduseid28@gmail.com>
2022-04-07 14:29:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - This prints the line
+ * Return: returns the line
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
