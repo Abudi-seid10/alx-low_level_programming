b0547a05654e7743301c09eca43258f183cf49de
Abudi-seid10 <abduseid28@gmail.com>
2022-04-08 12:30:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Definer
+ * Return: passer
+ */
+int main(void)
+{
+	char lcase = 'a';
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		if (lcase != 'q' && lcase != 'e'){
+			putchar(lcase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
