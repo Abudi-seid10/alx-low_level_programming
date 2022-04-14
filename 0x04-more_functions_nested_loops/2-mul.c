21a75d718ddbb4a4170233e0abeaa6f6322e9c74
Abudi-seid10 <abduseid28@gmail.com>
2022-04-14 14:33:20 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * _isdigit - determine if digit
+ *
+ * @c: character to determine
+ *
+ * Return: gives boolean
+ */
+int mul(int a, int b)
+{
+	int answer;
+	answer = a * b;
+	printf("%d \n", answer)
+
+	return (0);
+}
