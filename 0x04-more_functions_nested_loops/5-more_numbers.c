691ed51420fb0bae83f6bb691634fbaf6b5056aa
Abudi-seid10 <abduseid28@gmail.com>
2022-04-14 14:50:54 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers
+ *
+ */
+void more_numbers(void)
+{
+	int x = 0;
+	while(x < 10)
+	{
+		for (int j=0,j<16,j++)
+			_putchar(j);
+		_putchar("\n");
+		x++;
+	}
+	_putchar("\n");
+}
