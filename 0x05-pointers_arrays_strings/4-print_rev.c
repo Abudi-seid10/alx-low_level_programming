1a4019ef4f56f7f2252280294f139d4dac2fe465
Abudi-seid10 <abduseid28@gmail.com>
2022-04-18 17:01:02 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,22 @@
 #include "main.h"
 /**
- * _puts - prints a string followed by a new line
- * @str: input
+ * print_rev - prints a string, in reverse
+ * @s: input
+ * Return: the result
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		_putchar(str[i]);
 		i++;
+	}
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
